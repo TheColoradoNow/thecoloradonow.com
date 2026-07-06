e23e0f4deff9811047a0ca5894b8c91c05e66032
The Colorado Now <Julianhanes5@gmail.com>
2026-07-05 21:05:10 -0600

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,6 @@
 (function () {
   const SUPABASE_URL = 'https://ilhlwostiitksxhhaqbt.supabase.co';
-  const SUPABASE_ANON_KEY = 'PASTE_NEW_SUPABASE_ANON_PUBLIC_KEY_HERE';
+  const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlsaGx3b3N0aWl0a3N4aGhhcWJ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI4NDIyMzQsImV4cCI6MjA5ODQxODIzNH0.qlQSseaPMy2bYHxnR3xchAtmFTQ-gs1ndR85hhjaIrs';
 
   window.TheColoradoNow = window.TheColoradoNow || {};
   window.TheColoradoNow.supabaseUrl = SUPABASE_URL;
@@ -11,7 +11,7 @@
     return;
   }
 
-  if (SUPABASE_ANON_KEY === 'PASTE_NEW_SUPABASE_ANON_PUBLIC_KEY_HERE') {
+  if (SUPABASE_ANON_KEY === 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlsaGx3b3N0aWl0a3N4aGhhcWJ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI4NDIyMzQsImV4cCI6MjA5ODQxODIzNH0.qlQSseaPMy2bYHxnR3xchAtmFTQ-gs1ndR85hhjaIrs') {
     console.error('Add the new Supabase anon public key in supabase-config.js.');
     return;
   }
