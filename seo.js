c171e21134a94e9041779636389f3e551caa3df4
The Colorado Now <Julianhanes5@gmail.com>
2026-09-23 14:51:41 -0600

    Apply new Colorado Now brand colors and logo

--- seo.js
+++ seo.js
@@ -1,7 +1,7 @@
 (function () {
   'use strict';
   const base = 'https://thecoloradonow.com/';
-  const logo = base + 'colorado-now-logo-cropped-v2.png';
+  const logo = base + 'colorado-now-official-logo-2026.png';
 
   function meta(key, value) {
     const attribute = key.includes(':') && !key.startsWith('twitter:') ? 'property' : 'name';
